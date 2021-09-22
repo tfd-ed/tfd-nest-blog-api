2de801423373e56899869cc75cc1af0eb89105b0
Oberleutnant KimAng <kimangkheang@gmail.com>
2021-09-22 10:04:05 +0700

    Config for VS Code

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ]
   },
 };
