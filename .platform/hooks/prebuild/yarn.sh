090340ab3cb1f0a0e9ca4b36c5bec8da7612cc40
KimAng <kimangkheang@gmail.com>
2022-11-13 01:13:36 +0900

    Add yarn hook

---
+++ .platform/hooks/prebuild/yarn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# need to install node first to be able to install yarn (as at prebuild no node is present yet)
+sudo curl -silent -location https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum -y install nodejs
+# install yarn
+sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+sudo yum -y install yarn
+# install
+cd /var/app/staging/
+# debugging..
+ls -lah
+yarn
