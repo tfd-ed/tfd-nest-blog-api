a823f06dd48fbab89ad3878b700c80c5a71e8704
KimAng <kimangkheang@gmail.com>
2022-11-15 17:37:07 +0900

    Add cache username

--- src/modules/common/config.ts
+++ src/modules/common/config.ts
@@ -125,8 +125,9 @@ export async function throttlerConfig(configService: ConfigService) {
     redisObj = {
       host: db_host,
       port: db_port,
-      user: configService.get('CACHE_USER'),
+      username: configService.get('CACHE_USER'),
       password: db_password,
+      // url: configService.get('REDIS_URL'),
       tls: {
         servername: db_host,
         rejectUnauthorized: false,
